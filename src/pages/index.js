a5d9bac516ef0c1bd2312a09aca47165dd842b3c
Phantom077 <58046080+Phantom077@users.noreply.github.com>
2020-01-20 10:04:57 -0800

    video player

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ReactPlayer from "react-player"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" playing />
   </Layout>
 )
 
