22d8c9042fe933a89e4f4b179eef2c32f9822936
Phantom077 <58046080+Phantom077@users.noreply.github.com>
2020-01-20 10:11:53 -0800

    ResponsivePlayer component added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,27 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ReactPlayer from "react-player"
+import "../styles/app.css"
+
+class ResponsivePlayer extends React.Component {
+  render() {
+    return (
+      <div className="player-wrapper">
+        <ReactPlayer
+          className="react-player"
+          url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    )
+  }
+}
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" playing />
+    <ResponsivePlayer />
   </Layout>
 )
 
